5de12315475c234ff82a9e17703d5561cdfa53a9
Antho.lib <vdw.anthonylib@gmail.com>
2020-04-01 18:58:29 +0200

    Modif 01/04
    Ajout des dossiers MVC , modif index + ajout vueAccueil

---
+++ Vue/vueAccueil.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+$titre =  "<h2>Safethings</h2>";
+$contenu = "Protections sanitaires";
+
+$categories = "	<div id=\"presentation\">
+
+    <div class=\"possibilites\">
+      <img src=\"img/fond/ffp3.jpg\" alt=\"Configurations\" class=\"images\">
+      <h3>Protections du visage</h3>
+      <div class=\"middle\">
+      <div class=\"text\"><a href=\"\">Voir</a></div>
+      </div>
+    </div>
+    <div class=\"possibilites\">
+      <img src=\"img/fond/gel.jpg\" alt=\"Configurations\" class=\"images\">
+      <h3>Protections des mains</h3>
+      <div class=\"middle\">
+      <div class=\"text\"><a href=\"\">Voir</a></div>
+      </div>
+    </div>
+
+    <div class=\"possibilites\">
+      <img src=\"img/fond/combi.jpg\" alt=\"Configurations\" class=\"images\">
+      <h3>Protections diverses</h3>
+      <div class=\"middle\">
+      <div class=\"text\"><a href=\"\">Voir</a></div>
+      </div>
+    </div>
+
+  </div>";?>
+<?php
+include('template.php');
+?>
--- index.php
+++ index.php
@@ -1,34 +1,3 @@
-<?php
-session_start();
-$titre =  "<h2>Safethings</h2>";
-$contenu = "Protections sanitaires";
-
-$categories = "	<div id=\"presentation\">
-
-    <div class=\"possibilites\">
-      <img src=\"img/fond/ffp3.jpg\" alt=\"Configurations\" class=\"images\">
-      <h3>Protections du visage</h3>
-      <div class=\"middle\">
-      <div class=\"text\"><a href=\"\">Voir</a></div>
-      </div>
-    </div>
-    <div class=\"possibilites\">
-      <img src=\"img/fond/gel.jpg\" alt=\"Configurations\" class=\"images\">
-      <h3>Protections des mains</h3>
-      <div class=\"middle\">
-      <div class=\"text\"><a href=\"\">Voir</a></div>
-      </div>
-    </div>
-
-    <div class=\"possibilites\">
-      <img src=\"img/fond/combi.jpg\" alt=\"Configurations\" class=\"images\">
-      <h3>Protections diverses</h3>
-      <div class=\"middle\">
-      <div class=\"text\"><a href=\"\">Voir</a></div>
-      </div>
-    </div>
-
-  </div>";?>
-<?php
-include('template.php');
-?>
+<?php require 'vueAccueil.php';
+ ?>
+// ajouter routage
