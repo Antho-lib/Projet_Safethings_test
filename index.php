c108c201bc7381aefe0126fb283cf76e03b66a23
Antho.lib <vdw.anthonylib@gmail.com>
2020-04-15 16:02:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
-
-
-$url = '/monprojet/Projet_Safethings/';
-if (isset ($_GET['url'])) {
-    $url = explode('/',$_GET['url']);
+session_start();
+define('ROOT_PATH', "C:/xampp/www/monprojet/Projet_Safethings/"); //Racine du projet
+$request = str_replace(ROOT_PATH, "", $_SERVER['REQUEST_URI']);
+$request = parse_url($request, PHP_URL_PATH); //Parser pour ne pas garder les GET
+$segments = array_filter(explode('/', $request));
+if (!count($segments) or $segments[0] == 'index'){
+    $segments[0] = 'welcome';
 }
-
-
-if ($url == '') {
-  require 'welcome.php';
-} elseif ($url[0] = 'article' AND !empty($url[1])) {
-  echo "Voici ton article".$url[1];
-} else {
-  echo "Erreur 404";
+define('REQ_TYPE', $segments[0] ?? Null);
+define('REQ_TYPE_ID', $segments[1] ?? Null);
+define('REQ_ACTION', $segments[2] ?? Null);
+$file = 'controllers/'.REQ_TYPE.(REQACTION ? ''.REQ_ACTION : '').'.php';
+if(file_exists($file)){
+    require $file;
+    exit();
 }
-
-
-
-
-
- ?>
+else {
+    require 'controllers/404.php';
+    exit();
+}
+?>
